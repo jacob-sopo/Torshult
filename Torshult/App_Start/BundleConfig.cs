5b5b1ba112eff5378f82ca16abe6a4e53e687c44
Jacob Berg <jacob.berg@knowit.se>
2017-05-31 14:54:09 +0200

    Progress of page, added smoohtscroll and anchor points

--- Torshult/App_Start/BundleConfig.cs
+++ Torshult/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace Torshult
                 "~/Scripts/owl.carousel.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.smooth.scroll-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
