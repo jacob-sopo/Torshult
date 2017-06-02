5761657911ee317085bb19638b453c8489ab15c3
Jacob Berg <jacob.berg@knowit.se>
2017-06-02 10:42:09 +0200

    New architecture

--- Torshult/App_Start/BundleConfig.cs
+++ Torshult/App_Start/BundleConfig.cs
@@ -15,6 +15,9 @@ namespace Torshult
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.smooth.scroll-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/torshult").Include(
+                        "~/Scripts/torshult.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ Torshult/Scripts/Torshult.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $(".owl-carousel").owlCarousel(
+        {
+            items: 1,
+            itemsCustom: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            singleItem: false,
+            itemsScaleUp: false,
+            slideBy: 1
+        });
+});
+
+$('.smooth').on('click',
+    function () {
+        $.smoothScroll({
+            scrollElement: $('body'),
+            scrollTarget: '#' + this.id
+        });
+
+        return false;
+    });
